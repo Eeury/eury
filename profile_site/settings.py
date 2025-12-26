2084f8bdd46eba8d998a5a5382ed2875668107e2
Eeury <callmeeury@gmail.com>
2025-12-26 12:36:59 +0300

    waaa

--- profile_site/settings.py
+++ profile_site/settings.py
@@ -148,15 +148,17 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+#MEDIA_URL = 'media/'
+#MEDIA_ROOT = BASE_DIR / 'media'
 
 # Cloudinary configuration (values come from .env)
 cloudinary.config(
     cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
     api_key=os.getenv('CLOUDINARY_API_KEY'),
     api_secret=os.getenv('CLOUDINARY_API_SECRET'),
+    secure=True,
 )
+
 
 # Store uploaded media files on Cloudinary
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
